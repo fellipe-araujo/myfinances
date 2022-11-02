aec4b672b3c3b85785f69ceb258fe109b72752f9
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2022-11-02 16:21:27 -0300

    feat: create mask to input

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import MaskInput, { Masks, MaskInputProps } from 'react-native-mask-input';
 
 import { InputContainer } from './styles';
 
-export function Input() {
-  return <InputContainer  />;
+interface InputProps extends MaskInputProps {
+  masked?: boolean;
+}
+
+export function Input({ masked, ...rest }: InputProps) {
+  return (
+    <>
+      {masked ? (
+        <MaskInput
+          {...rest}
+          mask={Masks.BRL_CURRENCY}
+          style={{
+            flex: 1,
+            borderBottomColor: '#322030',
+            borderBottomWidth: 1,
+            marginTop: 10,
+            marginBottom: 10,
+            marginRight: 8,
+            marginLeft: 8,
+          }}
+        />
+      ) : (
+        <InputContainer {...rest} />
+      )}
+    </>
+  );
 }
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,10 +1,9 @@
 import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
-import { TextInputProps } from 'react-native';
 
-export const InputContainer = styled.TextInput<TextInputProps>`
-  width: ${RFValue(96)}px;
-  height: 100%;
+export const InputContainer = styled.TextInput`
+  flex: 1;
   border-bottom-color: #322030;
   border-bottom-width: 1px;
+  margin: 10px 8px;
 `;
